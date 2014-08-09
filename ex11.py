e3d8842ec21d0b889a05522cdcbd49a73e5308a9
JGoldsmith <justingoldsmith@my.tridenttech.edu>
2014-08-09 17:04:44 -0400

    two methods available. Run it and see.

--- ex11.py
+++ ex11.py
@@ -1,9 +1,20 @@
-print "How old are you?",
-age = raw_input ()
-print "How tall are you?",
-height = raw_input ()
-print "How much do you weight?",
-weight = raw_input ()
+def method_one():
+    print "How old are you?",
+    age = raw_input ()
+    print "How tall are you?",
+    height = raw_input ()
+    print "How much do you weigh?",
+    weight = raw_input ()
 
-print "So you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
+    print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+def method_two():
+    print "How old are you?",
+    age = raw_input()
+    print "How tall are you?",
+    height = raw_input()
+    print "How much do you weigh?",
+    weight = raw_input()
+
+    print "So you're " + age + " years old, " + height + " tall and " + weight + " lbs heavy."
