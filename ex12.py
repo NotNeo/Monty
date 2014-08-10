090358cd39c0bc48b747b771287ededfd2adbd7f
NotNeo <jamesanderson2nd@gmail.com>
2014-08-10 18:44:56 -0400

    Another Python The Hardway Exercise

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you?" )
+weight = raw_input("How much do you weight?" )
+
+print "So, you're %r years old, %r feet tall and you weigh %r lbs." % (age, height, weight)
