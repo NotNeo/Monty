e3caabb9d5ba9893b55820b1fde488d554a4b6ed
NotNeo <jamesanderson2nd@gmail.com>
2014-08-21 12:58:28 -0400

    First round of changes, very minor stuff, uploaded the changes before taking a break.

--- ex26.py
+++ ex26.py
@@ -1,4 +1,4 @@
-def break_words(stuff):
+def break_words(words):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
@@ -9,12 +9,12 @@ def sort_words(words):
 
 def print_first_word(words)
     """Prints the first word after popping it off."""
-    word = words.poop(0)
-    print word
+    word = words.pop(0)
+    print words
 
-def print_last_word(words):
+def print_last_word(word):
     """Prints the last word after popping it off."""
-    word = words.pop(-1
+    word = words.pop(-1)
     print word
 
 def sort_sentence(sentence):
@@ -25,14 +25,14 @@ def sort_sentence(sentence):
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
     words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
+    print first_word(words)
+    print last_word(words)
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
     words = sort_sentence(sentence)
-    print_first_word(words)
-    print_last_word(words)
+    print first_word(words)
+    print last_word(words)
 
 
 print "Let's practice everything."
@@ -74,7 +74,7 @@ print "We can also do that this way:"
 print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
 
 
-sentence = "All god\tthings come to those who weight."
+sentence = "All god\tthings come to those who wait."
 
 words = ex25.break_words(sentence)
 sorted_words = ex25.sort_words(words)
